1e60cfd05ed0702f0bbea13b17aa9a76b24162f3
ajcost <acost@sas.upenn.edu>
2017-06-25 23:03:03 -0400

    Indentation fixes
    More indentation fixes

--- Compression/CompressionToHDFS/src/main/java/edu/upenn/ajcost/CompressionToHDFS/App.java
+++ Compression/CompressionToHDFS/src/main/java/edu/upenn/ajcost/CompressionToHDFS/App.java
@@ -26,38 +26,38 @@ public class App {
 	 * 
 	 **/
 	public static void writePoststoHDFS(SparkSession spark, Dataset<Row> listData, String input, String locationPrefix) {
-    	Dataset<Row> list = listData.toDF();
-    	list = list.toDF().filter(functions.col("author").notEqual("[deleted]"));
-    	list.write().parquet(locationPrefix + input + ".parquet");
-    	list = null;
-	}
-	
-	public static void main(String[] args) {
-		
+       Dataset<Row> list = listData.toDF();
+       list = list.toDF().filter(functions.col("author").notEqual("[deleted]"));
+       list.write().parquet(locationPrefix + input + ".parquet");
+       list = null;
+   }
+   
+   public static void main(String[] args) {
+      
     	// Configure and Start Spark Session
-    	SparkSession spark = SparkSession
-    			.builder()
-    			.appName("CompressionToHDFS")
-    			.getOrCreate();
-    	
-		String submissionFilePath = "s3a://ac-reddit-data/Raw/" + args[0];
-		String commentFilePath = "s3a://ac-reddit-data/Raw/" + args[1];
-		
-    	Dataset<Row> postList = spark.read().json(submissionFilePath).persist(StorageLevel.MEMORY_AND_DISK());
-    	Dataset<Row> postListData = postList.toDF()
-    			.select(new Column("author"),
-    					new Column("subreddit"),
-    					new Column("id"));
-    	
-		writePoststoHDFS(spark, postListData, args[0], args[2]);
-		
-    	Dataset<Row> commentList = spark.read().json(commentFilePath).persist(StorageLevel.MEMORY_AND_DISK());
-    	Dataset<Row> commentListData = commentList.toDF()
-    			.select(new Column("author"),
-    					new Column("subreddit"),
-    					functions.regexp_replace(commentList.col("link_id"), "t3_", "").as("id"));
-		
-		writePoststoHDFS(spark, commentListData, args[1], args[2]);
-    	
-	}
+       SparkSession spark = SparkSession
+       .builder()
+       .appName("CompressionToHDFS")
+       .getOrCreate();
+       
+       String submissionFilePath = "s3a://ac-reddit-data/Raw/" + args[0];
+       String commentFilePath = "s3a://ac-reddit-data/Raw/" + args[1];
+       
+       Dataset<Row> postList = spark.read().json(submissionFilePath).persist(StorageLevel.MEMORY_AND_DISK());
+       Dataset<Row> postListData = postList.toDF()
+       .select(new Column("author"),
+           new Column("subreddit"),
+           new Column("id"));
+       
+       writePoststoHDFS(spark, postListData, args[0], args[2]);
+       
+       Dataset<Row> commentList = spark.read().json(commentFilePath).persist(StorageLevel.MEMORY_AND_DISK());
+       Dataset<Row> commentListData = commentList.toDF()
+       .select(new Column("author"),
+           new Column("subreddit"),
+           functions.regexp_replace(commentList.col("link_id"), "t3_", "").as("id"));
+       
+       writePoststoHDFS(spark, commentListData, args[1], args[2]);
+       
+   }
 }
